1df5923488b26149e2c05a4c4b2fd32bfb21b538
Chandler Corn <rochala827@gmail.com>
2020-02-07 16:54:09 -0500

    Added more extensive comments
    (title)

--- 3mm.py
+++ 3mm.py
@@ -5,16 +5,18 @@ import player as pl
 
 
 class TMM(Tk):
+    # set of edges
+    # each edge is the pos on the board that your coming from and the position that you could go to
+    # i.e edge [0,1] shows that the position on the graphical board is connected to position 1 on the graphical board
     edges = [{0, 1}, {1, 2}, {3, 4}, {4, 5}, {6, 7}, {7, 8}, {0, 3}, {3, 6},
              {1, 4}, {4, 7}, {2, 5}, {5, 8}, {0, 4}, {2, 4}, {6, 4}, {8, 4}]
-
+    # list of winning positions
     winners = ((0, 1, 2), (3, 4, 5), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6), (6, 7, 8))
 
     def __init__(self):
         self.parent = Tk.__init__(self)
 
         # make lists of all 27 images: 9 for empty square, 9 for blue, 9 for red
-
         self.emptypix = []
         self.redpix = []
         self.bluepix = []
@@ -23,9 +25,12 @@ class TMM(Tk):
             self.redpix.append(PhotoImage(file=f"Images/r{i}.gif"))
             self.bluepix.append(PhotoImage(file=f"Images/b{i}.gif"))
 
-        self.board = [1, 1, 1, 0, 0, 0, 2, 2, 2]  # starting board configuration
-        self.blist = []  # list of buttons
+        # starting board configuration
+        self.board = [1, 1, 1, 0, 0, 0, 2, 2, 2]
+        # list of buttons
+        self.blist = []
 
+        # fill the graphical board with images from the directory 3MMAlphaBetaPruning/Images
         for i in range(9):
             if self.board[i] == 1:  # 1 is blue
                 bu = Button(self, image=self.bluepix[i])
@@ -33,24 +38,22 @@ class TMM(Tk):
                 bu = Button(self, image=self.redpix[i])
             else:  # 0 is empty
                 bu = Button(self, image=self.emptypix[i])
-
-            bu.configure(command=lambda b=bu: self.click(b))  # the lambda trick: identify the button
-            bu.position = i  # store button number in the object itself
-            bu.grid(row=(i // 3), column=(i % 3))  # grid coordinates
-            self.blist.append(bu)  # append to button list
+            # the lambda trick: used to identify the button since this is not able to be down with tkinter alone
+            bu.configure(command=lambda b=bu: self.click(b))
+            # store button number in the object itself
+            bu.position = i
+            # grid coordinates
+            bu.grid(row=(i // 3), column=(i % 3))
+            # append to button list
+            self.blist.append(bu)
 
         self.moving = False  # start the game, moving is True after we pick the piece to move
-        self.ai_player = 2  # 2 if you want ai to be red 1 if blue
-        self.turn = 1  # red moves first if 2 blue if 1
-        #makes first move using player module
-       # bcopy = [-1 if x == 2 else x for x in self.board]
-       # move, _ = pl.beta(bcopy, 0)
-       # self.do_move(move[0], move[1], self.turn)
-
+        self.ai_player = 2   # 2 if you want ai to be red 1 if blue
+        self.turn = 1        # red moves first if 2 blue if 1
+    # function checkgame checks if the game is over after a move returns who won or if the game is not over returns 0
     def checkgame(self):
-        who = self.turn
-
-        #if no possible moves exist then the other player wins
+        who = self.turn # assigns current player to who
+        # if no possible moves exist then the other player wins
         if(len(pl.posmovgenerator(self.board,who)) == 0):
             return 3 - who
 
@@ -67,12 +70,14 @@ class TMM(Tk):
         return 0
 
 
-    # the do_move method is not called until we know the move is valid
-    # new pos is where the player is going oldpos is where the circle is coming from
+    # do move executes a givene move using the new position(newpos) and the old position(oldpos) and the current player(who)
     def do_move(self, newpos, oldpos, who):
         self.blist[oldpos].configure(image=self.emptypix[oldpos])  # put empty image at old location
-        self.board[oldpos] = 0  # update the board
+        self.board[oldpos] = 0                                     # update the board
         self.board[newpos] = who
+
+        # if the player is blue replace square with blue dot
+        # else the player is red so replace the square on the grpahical board with a red dot
         if who == 1:
             self.blist[newpos].configure(image=self.bluepix[newpos])
         else:
@@ -80,25 +85,24 @@ class TMM(Tk):
 
         # Checks if game is over 2 = red win 1 = blue win 0 = gamenotover
         retval = self.checkgame()
-        # reset function needs setup
         if retval != 0:
-
             self.gameover = True
+            # resets the board
             self.reset()
-
+            # calls the popupmsg depending on who won
             if retval == 1:
                 self.popupmsg("BLUE WINS")
             if retval == 2:
                 self.popupmsg("RED WINS")
-
         self.moving = False
         self.turn = 3 - who
         if self.turn == 2:
+            # calls beta from player.py that starts the alpha beta pruning for the ai player
             move, _ = pl.beta(self.board, 0)
             self.do_move(move[1], move[0], self.turn)
 
 
-    # popup message function not quite fully understood make sure to look over.
+    # creates a popup msg to shgow who has won the match
     def popupmsg(self, msg):
         popup = Tk()
         popup.wm_title("VICTORY MESSAGE")
@@ -108,7 +112,7 @@ class TMM(Tk):
         okayButton.pack()
         popup.mainloop()
 
-    # reset the board
+    # Resets the board to its default settings so a new game can start
     def reset(self):
         self.board = [1, 1, 1, 0, 0, 0, 2, 2, 2]
         i = 0
@@ -122,11 +126,9 @@ class TMM(Tk):
         while i < 9:
             self.blist[i].configure(image=self.redpix[i])
             i += 1
+
         self.moving = False
         self.turn = 1
-       # bcopy = [-1 if x == 2 else x for x in self.board]
-       # move, _ = pl.beta(bcopy, 0)
-        #self.do_move(move[0], move[1], self.turn)
 
     def click(self, button):
         x = button.position  # number of the button that was clicked
@@ -137,7 +139,6 @@ class TMM(Tk):
             if self.board[x] == self.turn:  # need a reminder of old position
                 self.savepos = x
                 self.moving = True
-
 
 if __name__ == "__main__":
     game = TMM()
--- player.py
+++ player.py
@@ -1,18 +1,21 @@
 #!usr/bin/en python3
 
 from random import choice,shuffle
+    # set of edges
+    # each edge is the pos on the board that your coming from and the position that you could go to
+    # i.e edge [0,1] shows that the position on the graphical board is connected to position 1 on the graphical board
 edges = [{0, 1}, {1, 2}, {3, 4}, {4, 5}, {6, 7}, {7, 8}, {0, 3}, {3, 6},
          {1, 4}, {4, 7}, {2, 5}, {5, 8}, {0, 4}, {2, 4}, {6, 4}, {8, 4}]
-
+    # list of winning positions
 winners = ((0, 1, 2), (3, 4, 5), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6), (6, 7, 8))
 def boardvalue(board, who):
-    #board value for red player
+    # board value for alpha player
     if who == -1:
         for pat in winners[:-1]:
             tmp = {board[x] for x in pat}
-            if len(tmp) == 1 and who in tmp:
+            if len(tmp) == 1 and who in tmp: # if the new move caused a victory position return the value as who
                 return who
-    #board value if beta board
+    # board value if beta board
     if who == 1:
         for pat in winners[1:]:
             tmp = {board[x] for x in pat}
@@ -27,21 +30,21 @@ def beta(bcopy, depth):     # considering red's moves
     movelist = posmovgenerator(bcopy,-1)
     # best value may need increased to allow more parameters for tghe value of moves?
     bestval = 2
-
+    # shuffle the movelist so that it is randomized before considering the moves
     shuffle(movelist)
 
     # for each move in the movelist make a move that is randomized by the shuffle above.
     # make that move on the copy board and check the value of the board
     # use this return value choose what to do with the move
     for move in movelist:
-        bcopy[move[0]] = 0
+        bcopy[move[0]] = 0  # make a move onto the copy board that can be evaluated
         bcopy[move[1]] = -1
-        val = boardvalue(bcopy, -1)
-        if val == 0 and depth < 4:
+        val = boardvalue(bcopy, -1) # check the value of this move
+        if val == 0 and depth < 4: # value returns 0 and the depth allows us to continue call alpha
             _, val = alpha(bcopy,depth+1)
         if val < bestval:
             bestmove, bestval = move, val
-        bcopy[move[0]] = -1
+        bcopy[move[0]] = -1 # undo the move from the board copy
         bcopy[move[1]] = 0
     return bestmove, bestval
 
@@ -62,13 +65,14 @@ def alpha(bcopy, depth):     # considering red's moves
         bcopy[move[1]] = 0
     return bestmove, bestval
 
+# this function generates a list of possible moves that can be made for a board and the player
 def posmovgenerator(bcopy, who):
     posMov = []
     x = 0
     while x < 9:
         i = 0
         while i < 9:
-            if bcopy[x] == who and {x, i} in edges:
+            if bcopy[x] == who and {x, i} in edges: # if the position is controlled by who and the newpos is empty append
                 if bcopy[i] == 0:
                     posMov.append([x, i])
             i += 1
